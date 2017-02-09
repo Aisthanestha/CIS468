468518e98d6407ef6914b38c7e5a32aee02eab22
aisthanestha <ghendrickson47@gmail.com>
2017-02-09 12:23:27 -0600

    tryit3,py added dict type instead of set

--- python/tryits/week2/tryit3.py
+++ python/tryits/week2/tryit3.py
@@ -5,6 +5,7 @@ import json
 with open('tweets.json') as f:
         #read in lines as strings u''
         data=f.readlines()
+'''
 #create tz as a set
 tz=set()
 for i in data:
@@ -14,9 +15,15 @@ for i in data:
         t=x['user']['time_zone']
         #adds t to the set
         tz.add(t)
-
-
-
+'''
+#add to a dictionary not a set >.>
+tz={}
+for i in data:
+        x=json.loads(i)
+        t=x['user']['time_zone']
+        #takes t and adds it to the dictionary timezone will be the key and 1 will be its value but the value of the key is irrelevant in this case...
+        tz[t]=1
+'''
 #this prints the set 
 #print(tz)
 
@@ -28,5 +35,18 @@ num1=len(tz)-1
 #so i just removed 1 as NONE =1 in the nnumber of timezones
 print('\nNote that NONE is a timezone listed in the tweets.json, without this it is a total of %s\n' %num1)
 #print('\ni am also assuming that Arizona, as it is listed in the json as is an time zone itself aswell')
+for i in tz:
+        print(i)
+
+'''
+# to see the keys 
+#print(tz.keys())
+num=len(tz.keys())
+print('\n')
+print('Total Number of time Zones:%s\n'%num)
+num-=1
+print('If NONE is considered not a TIMEZONE:%s\n'%num)
+
+
 for i in tz:
         print(i)
