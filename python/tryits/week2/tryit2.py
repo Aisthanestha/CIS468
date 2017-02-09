5183c5c1f862152d593a4320978b18d7816cbdbf
aisthanestha <ghendrickson47@gmail.com>
2017-02-09 10:25:42 -0600

    removed tryit2v2.py and fixed tryit2.py

--- python/tryits/week2/tryit2.py
+++ python/tryits/week2/tryit2.py
@@ -1,21 +1,46 @@
-import statistics
-
-def average(Students{}):
-        print(statistics.mean.Students['tyler'])
-
-scoreInput=''
-Students={'tyler':[10,100,340,20,10],'greg':[100,100,54,72,57]}
-#pprint.pprint(Students)
-
-print(Students.keys())
-
-userInput=input('Enter a students name, q to quit: ')
-
-if userInput in Students.keys():
-        scoreInput=input('Enter the score for the student: ')
-
-elif userInput == 'q':
-        print()
+#!/usr/bin/env python
+def averager(var):
+        var=list(map(int, var))
+        #this will add them together
+        sum(var)
+        #this will tell us the # of values in the list
+        len(var)
+        #basic math
+        avg=float(sum(var)/len(var))
+        return avg
+                        
+                        
+                        
+                        
+#make variable in dict a list so we can add stuff to it
+hisScore = ['100','95','68','84']
+myScore=['100','100','100']
+#create dict, name is key hiscore and myscore are lists to be able to append
+students={'tyler': hisScore, 'greg': myScore}
+#lets make an entry, to my understanding []
+studentName=input('Enter the student name: ')
+#check if in dict
+#throws error score does not exist otherwise
+score=''
+#check if studentName is within students key list
+if studentName in students.keys():
+        #prints current values of whatever student was chosen
+        print(students.get(studentName))
+        #while score doesn't equal q keep adding 
+        while score!='q':
+            #take user input for the score you are going to add
+            score=input('What was your score,[q] to quit: ')
+            #this if statement prevents q being appended to the list
+            if score!='q':
+            #we got this far lets add what our input was to the list    
+                    students[studentName].append(score)
+#lets them know if it is not a student
 else:
-        print('Invalid Key')
-average(Students)
+        print('invalid key')
+var=students[studentName]
+averaged=averager(var)
+print(averaged)
+
+
+#prints the dictionary so we can see what happened
+#print(students)
