a49dc527382965ae61ec0088f32a6448272ac317
aisthanestha <ghendrickson47@gmail.com>
2017-02-09 10:41:11 -0600

    added tryit3.py and commented on tryit1.py

--- python/tryits/week2/tryit1.py
+++ python/tryits/week2/tryit1.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
-
+#imports for use of random.randint
 import random
-
+#fucntion to create random nums
 def numGenerator():
+        #declar lis as a  list
         lis=list()
+        #run it 4 time and append an int into the list
         for x in range(0,4):
                 lis.append(random.randint(0,1000))
+        #this makes our lis which was a list to a tuple!
         tup1=tuple(lis)        
+        #return the tuple
         return tup1
 
-
+#nums equals the tup1 since thats what it returned
 nums=numGenerator()
+#print the nums from numGenerator
 print(nums)
 #print(type(nums))
